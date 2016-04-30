97b56b935d81814160775d912e161206fe97958a
Mahaboobsab Nadaf <mrnadaf2010@gmail.com>
2016-04-30 09:18:21 +0530

    Initial commit

---
+++ AlertMessage2/AlertMessage2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertMessage2
+//
+//  Created by test on 4/2/16.
+//  Copyright © 2016 com.NeoRays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlertMessage2/AlertMessage2/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  AlertMessage2
+//
+//  Created by test on 4/2/16.
+//  Copyright © 2016 com.NeoRays. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showMessage:(id)sender {
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"What is your phone number?"
+                                                      message:nil
+                                                     delegate:self
+                                            cancelButtonTitle:@"Cancel"
+                                            otherButtonTitles:@"Continue", nil];
+    
+    [message setAlertViewStyle:UIAlertViewStylePlainTextInput];
+    
+    [message show];
+}
+
+- (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView
+{
+    NSString *inputText = [[alertView textFieldAtIndex:0] text];
+    if( [inputText length] >= 10 )
+    {
+        return YES;
+    }
+    else
+    {
+        return NO;
+    }
+}
+
+
+
+@end
